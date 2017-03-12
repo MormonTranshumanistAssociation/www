953f10ff202a471f04dc673a697718872d466fa7
Nathan Hadfield <hadfieldn@gmail.com>
2017-03-11 17:19:35 -0700

    Add YouTubePlayer and MandateSection

--- app/containers/HomePage/sections/MandateSection/index.js
+++ app/containers/HomePage/sections/MandateSection/index.js
@@ -3,6 +3,7 @@ import { FormattedMessage }from 'react-intl';
 import Section from 'components/Section';
 import SectionContent from 'components/SectionContent'
 import styled from 'styled-components';
+import YouTubePlayer from 'components/YouTubePlayer';
 import messages from './messages';
 import LeavesImage from './leaves.jpg';
 
@@ -20,6 +21,11 @@ const TextWrapper = styled.div`
   padding: 0 2rem;
 `;
 
+const VideoWrapper = styled.div`
+  max-width: calc(384px + 2 * 2rem);
+  margin-bottom: 2rem;
+`;
+
 const MandateSection = (props) => {
   const affs = [1, 2, 3, 4, 5, 6 ].map((index) => {
     return {
@@ -31,6 +37,7 @@ const MandateSection = (props) => {
   return (
     <Section img={LeavesImage}>
       <SectionContent>
+        <VideoWrapper><YouTubePlayer videoId="VePRByRNIAc" /></VideoWrapper>
         <TextWrapper>
           <H><FormattedMessage {...messages.header} /></H>
           <p><FormattedMessage {...messages.text} /></p>
