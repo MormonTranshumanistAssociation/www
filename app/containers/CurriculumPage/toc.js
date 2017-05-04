044c89862aadeaee603b32124289ded8f451a414
Nathan Hadfield <hadfieldn@gmail.com>
2017-05-04 09:43:17 -0600

    Use primer title in ToC

--- app/containers/CurriculumPage/toc.js
+++ app/containers/CurriculumPage/toc.js
@@ -3,8 +3,8 @@ import TableOfContents, { Entry } from 'components/TableOfContents';
 
 const ToC = () => (
   <TableOfContents>
-    <Entry to="/primers/1">Primer 1</Entry>
-    <Entry to="/primers/2">Primer 2</Entry>
+    <Entry to="/primers/1">The Basics of Mormon Transhumanism</Entry>
+    <Entry to="/primers/2">The Purpose of the Mormon Transhumanist Association</Entry>
   </TableOfContents>
 );
 
