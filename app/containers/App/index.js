a6db180091f9b8f6a9223a078369d9dd7d685188
Nathan Hadfield <hadfieldn@gmail.com>
2018-03-17 23:34:00 -0600

    disable service worker

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -11,6 +11,33 @@ import Helmet from 'react-helmet';
 import styled from 'styled-components';
 import Footer from 'components/Footer';
 import withProgressBar from 'components/ProgressBar';
+// import runtime from 'offline-plugin/runtime';
+
+// // Automatic ServiceWorker updates
+// // see https://github.com/NekR/offline-plugin/blob/master/docs/updates.md
+// runtime.install({
+//   onUpdating: () => {
+//     // console.log('SW Event:', 'onUpdating');
+//   },
+//   onUpdateReady: () => {
+//     // console.log('SW Event:', 'onUpdateReady');
+//     // Tells to new SW to take control immediately
+//     runtime.applyUpdate();
+//   },
+//   onUpdated: () => {
+//     // console.log('SW Event:', 'onUpdated');
+//     // Reload the webpage to load into the new version
+//     window.location.reload();
+//   },
+//
+//   onUpdateFailed: () => {
+//     // console.log('SW Event:', 'onUpdateFailed');
+//   },
+// });
+//
+// // clear all SW caches
+// caches.keys().then((cs) => cs.forEach( (c) => caches.delete(c)));
+
 
 const AppWrapper = styled.div`
   margin: 0;
